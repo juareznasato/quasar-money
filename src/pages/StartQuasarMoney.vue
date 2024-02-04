ac253b4c513f1e89ec9a139fd9387ebb4e80d2f3
“juarez” <juareznasato@gmail.com>
2024-02-04 14:18:13 -0300

    Ajusted lay-out

--- src/pages/StartQuasarMoney.vue
+++ src/pages/StartQuasarMoney.vue
@@ -1,15 +1,28 @@
 <template>
   <br />
-  <quasar-money
-    ref="refQuasarMoney"
-    v-model="value"
-    v-bind:label="'Quasar Money'"
-    v-bind:prefix="'R$'"
-    v-bind:properties="quasarMoney.properties"
-    v-bind:options="quasarMoney.options"
-    v-bind:negativeNumber="true"
-  ></quasar-money>
-  value: {{ value }}
+  <div align="center">
+    <q-card flat bordered style="height: calc(100vh - 800px); width: 400px">
+      <br />
+      <q-card-section>
+        <q-scroll-area style="height: calc(100vh - 400px); max-width: 900px">
+          <div class="row q-col-gutter-md">
+            <div class="col-xs-12 col-sm-12 col-md-12">
+              <quasar-money
+                ref="refQuasarMoney"
+                v-model="value"
+                v-bind:label="'Quasar Money'"
+                v-bind:prefix="'R$'"
+                v-bind:properties="quasarMoney.properties"
+                v-bind:options="quasarMoney.options"
+                v-bind:negativeNumber="true"
+              ></quasar-money>
+              v-model value: {{ value }}
+            </div>
+          </div>
+        </q-scroll-area>
+      </q-card-section>
+    </q-card>
+  </div>
 </template>
 
 <script setup>
